0a180a34c375c6dbecfae24829a4547fdb891530
shoney <ddmczp@naver.com>
2023-03-22 16:45:14 +0900

    [feat] logo 클릭 시 메인페이지로 이동

--- src/App.js
+++ src/App.js
@@ -4,21 +4,33 @@ import MainPageComponent from "./main";
 import { Switch, Route } from "react-router-dom";
 import UploadPage from "./upload";
 import ArtPage from "./art";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact={true} path="/">
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path="/arts/:id">
-          <ArtPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/mamulogo2.png" alt="mamu logo" />
+          </Link>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/arts/:id">
+            <ArtPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+
+      <div id="footer"></div>
     </div>
   );
 }
