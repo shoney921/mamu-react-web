a72bf0b558197fd828dc6f13abda5d0bb3101673
shoney <ddmczp@naver.com>
2023-03-27 00:31:44 +0900

    [fix] main index.js 에서 이미지 url 상대 경로로 변경

--- src/main/index.js
+++ src/main/index.js
@@ -4,14 +4,14 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-
+import { API_URL } from "../config/constants.js";
 dayjs.extend(relativeTime);
 
 function MainPage() {
   const [arts, setArts] = React.useState([]);
   React.useEffect(function() {
     axios
-      .get("http://localhost:8080/arts")
+      .get(`${API_URL}/arts`)
       .then(function(result) {
         console.log(result);
         const arts = result.data.arts;
@@ -34,7 +34,11 @@ function MainPage() {
             <div className="art-card">
               <Link className="art-link" to={`/arts/${art.id}`}>
                 <div>
-                  <img className="art-img" src={art.imageUrl} alt="art img" />
+                  <img
+                    className="art-img"
+                    src={`${API_URL}/${art.imageUrl}`}
+                    alt="art img"
+                  />
                 </div>
                 <div className="art-contents">
                   <span className="artist-name">{art.artistName}</span>
